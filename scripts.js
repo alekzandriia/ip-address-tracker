1b331e581e259643662caa0b5c4d4ebed9900405
Alekzandriia <alekzoreillyz@me.com>
2022-11-06 14:39:21 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,20 +3,20 @@ const locations = document.getElementById('location');
 const timezone = document.getElementById('timezone');
 const isp = document.getElementById('isp');
 const search = document.getElementById('search');
-let lat = 43.6532;
-let lng = -79.3832;
+let lat;
+let lng;
 
-// Map
-const map = L.map('map').setView([`${lat}`, `${lng}`], 13);
+// init map with current geolocation data
+const map = L.map('map')
+map.locate({setView:true, watch:false, maxZoom: 17})
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-// ip tracker
-
-search.addEventListener('submit', function(e) {
+// get ip location info & update map location
+function locate (e) {
   e.preventDefault()
 
   var requestOptions = {
@@ -24,8 +24,10 @@ search.addEventListener('submit', function(e) {
     redirect: 'follow'
   };
 
+  // get value that user types in the form input
   let userQuery = document.getElementById('search-input').value
 
+  // search api with user query
   fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_41hfunnlRDf1tDkDTlA8vkxY0h0Gb&ipAddress=${userQuery}`, requestOptions)
     .then((response) => response.json())
     .then((result) => {
@@ -38,7 +40,13 @@ search.addEventListener('submit', function(e) {
       map.setView([`${lat}`, `${lng}`], 13)
     })
     .catch(error => console.log('error', error));
-})
+}
+
+// Get ip info on page load
+window.addEventListener('load', locate)
+
+// Update ip info on form submit
+search.addEventListener('submit', locate)
 
 
 
